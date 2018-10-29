6150149a7b05b7c89ecca1b333993e789811ac16
enhao.liu <enhao.liu@chinacache.com>
2018-10-29 22:19:55 +0800

    init gitbook

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+cd _book
+git init
+git checkout -b gh-pages
+git add .
+git commit -am "Update Book"
+git push https://github.com/haozi4go/Spring-Boot-Admin-Reference-Guide.git gh-pages --force"
+cd ..
